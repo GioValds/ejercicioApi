14a16120cc45043593b5e4cb730f69098d89ecfb
GioValds <albertovv.con22@utsjr.edu.mx>
2025-02-24 22:19:10 -0600

    first c

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { PokemonComponent } from './pages/pokemon/pokemon.component'; // Nueva ruta para Pokémon
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'pokemon', // Nueva ruta para Pokémon
+    component: PokemonComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'home'
+  }
+];
